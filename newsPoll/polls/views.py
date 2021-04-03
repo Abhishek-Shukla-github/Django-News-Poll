574094c9883cfde098295cd3ba622c84806bc7d3
abhishek-shukla-github <abhishekshuklaratedr@gmail.com>
2021-04-03 20:43:43 +0530

    Complete 404 and templates

--- newsPoll/polls/views.py
+++ newsPoll/polls/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Question
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("This is the Poll App at index!")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're Looking at question %s" % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question Does not exist!")
+    return render(request, 'polls/detail.html', {question: question})
 
 
 def results(request, question_id):
